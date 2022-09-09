3ff87185bfcf1167377a11f350e862a6ffc2c64a
BT616 <110855674+BT616@users.noreply.github.com>
2022-09-09 14:34:07 -0700

    moves pssword="", inside passowrdFUntciton

--- style.js
+++ style.js
@@ -4,7 +4,7 @@
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numeric = "1234567890";
-var special = "!@#$%^&*()_+"
+var special = "!@#$%^&*()_+";
 
 var passwordlength ="";
 var lowercaseSelect;
@@ -12,47 +12,50 @@ var uppercaseSelect;
 var numericSelect;
 var specialSelect;
 
-var password="";
+
 var passwordChar ="";
 
 function generatePassword(){
 
+var password="";
+
 var passwordLength = window.prompt("How many characters? Enter from 8-128");
 passwordLength = parseInt(passwordLength);
 
-
-
+while(passwordLength <= 8|| passwordLength >=128){
+    alert("must enter a number between 8-128")
+    return;
+  
+};
 
 var lowercaseSelect = confirm("do you want lowercase?");
 
 if (lowercaseSelect){
     passwordChar += lowercase;
-}
+}; 
 var uppercaseSelect = confirm("do you want uppercase?");
 
 if (uppercaseSelect){
     passwordChar += uppercase;
-}
+};
 var numericSelect = confirm("do you want numbers?");
 
 if (numericSelect){
     passwordChar += numeric;
-}
+};
 var specialSelect = confirm("do you want symbols?");
 
 if (specialSelect){
     passwordChar += special;
-}
-console.log(passwordChar)
+};
+
 
 
 for (var i = 0; i < passwordLength; i++) {
 password= password + passwordChar[Math.floor(Math.random() * passwordChar.length)]
-console.log(password)
 }
-return password;
-  
-  }
+  return password;
+  };
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -62,10 +65,12 @@ function writePassword(){
 var password = generatePassword();
 var passwordText = document.querySelector("#password");
 
-
 passwordText.value = password;
-}
+
+};
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click",writePassword);
+
+
